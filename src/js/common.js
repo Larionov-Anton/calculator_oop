9cd37f798c6bc9d90280c9b09de0dd9f727bce98
Larionov-Anton <larionovanton05@gmail.com>
2022-07-18 10:12:12 +0300

    Finish application

--- src/js/common.js
+++ src/js/common.js
@@ -27,9 +27,35 @@ let screen = document.querySelector('.calc-screen p');
 let numbers = ['0', '1', '2','3', '4', '5','6', '7', '8', '9', '.'];
 let mathActions = ['+/-', '%', '/', 'x', '-', '+'];
 
+let error = false;
+let finish = false;
+
 // Отливливает событие нажатия на все кнопки
 
-let buttons = document.querySelector('.calc-buttons').addEventListener('click', main);
+document.querySelector('.ac').addEventListener('click', clear);
+document.querySelector('.calc-buttons').addEventListener('click', main);
+
+
+// Отслеживает переполнение дисплея
+
+function screenLength(string) {
+	let length = string.length;
+	if (length < 7) {
+		screen.style.fontSize = '4rem';
+	} else if (length < 10)
+	{
+		screen.style.fontSize = '3rem';
+	} else if (length < 13) {
+		screen.style.fontSize = '2.5rem';
+	} else if (length < 16) {
+		screen.style.fontSize = '2rem';
+	} else if (length < 21) {
+		screen.style.fontSize = '1.5rem';
+	} else 
+	{
+		return error = true;
+	}
+};
 
 
 // Сбросить все значения
@@ -39,85 +65,92 @@ function clear() {
 	b = '';
 	mathSign = '';
 	screen.textContent = '0';
+	finish = false;
+	error = false;
 };
 
-// Сбросить значения после вычисления
-
-function clearEqual() {
-	b = '';
-	mathSign = '';
-	screen.textContent = a;
-};
 
 
 // Математические вычисления
 
 function equal() {
+	if (b === '') b = a;
 	switch (mathSign) {
 		case '+': 
 			a = (+a) + (+b);
-			clearEqual();
 			break;
 		case '-':
 			a = a - b;
-			clearEqual();
 			break;
 		case '/':
 			a = a / b;
-			clearEqual();
 			break;
 		case 'x':
 			a = a * b;
-			clearEqual();
 			break;
 		case '+/-':
 			a = -a;	
 			break;
 		case '%':
 			a = a / 100 * b;
-			clearEqual();
 			break;
 	}
+
+	finish = true;
+	screen.textContent = a;
 };
 
 // Основной функционал 
 
 function main() {
-	let element = event.target.textContent;
-	let number = numbers.includes(element);
-	let mathAction = mathActions.includes(element);
+	let char = event.target.textContent;
+	let number = numbers.includes(char);
+	let mathAction = mathActions.includes(char);
 
-	if (element === 'ac') {
-		clear();
-	} else
+			if(!event.target.classList.contains('btn')) return;
+			if(event.target.classList.contains('ac')) return;
 
-	if (number && mathSign === '' ) {
-		a += element;
-		screen.textContent = a;
-	} else 
+			
+			if (number) {
 
-	if (mathAction) {
-		mathSign = element;
-		if (mathSign === '+/-') {
-			equal(); 
-		}
-		screen.textContent = a ;
-	}  else 
+				screenLength(screen.textContent);
 
-	if (element === '=') {
-		equal();
-	} else
+				if (b === '' && mathSign === '' && !error) {
+					a += char;
+					screen.textContent = a;
+				} 
+				else if (a!== '' && b!== '' && finish) {
+					b = char;
+					finish = false;
+					screen.textContent = b;
+				}
+				else if(!error) {
+					b += char;
+					screen.textContent = b;
+				}
 
-	{
-		b += element;
-		screen.textContent = b;
-	}
-	
+				return;
+			};
 
-	console.log(a, mathSign, b);
-}
+			if (mathAction) {
+				mathSign = char;
+				error = false;
+				if (char === '+/-') {
+					equal();
+				} else {
+					screen.textContent = mathSign;
+				}
+				
+				return;
+			};
+
+			if (char === '=' ) {
+				equal();
+
+				return;
+			};
 
 
-
+};
 
 
